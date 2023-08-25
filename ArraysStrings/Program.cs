38d31331bd86cd6230249c5446bbca2927de4218
xrlogic1 <xrlogic1@gmail.com>
2023-08-25 21:31:59 +0400

    *

--- ArraysStrings/Program.cs
+++ ArraysStrings/Program.cs
@@ -61,26 +61,142 @@
 // using PNSE = System.PlatformNotSupportedException;
 
 ///Random random = new Random();
-var random = new Random();
-var passwordBuilder = new StringBuilder();
+//var random = new Random();
+//var passwordBuilder = new StringBuilder();
 
-for (int i = 0; i < 12; i++)
+//for (int i = 0; i < 12; i++)
+//{
+//    int isDigit = random.Next(0, 2);
+//    char c;
+
+//    if (isDigit == 1)
+//    {
+//        c = (char)random.Next(48, 57 + 1);
+//    }
+//    else
+//    {
+//        c = (char)random.Next(97, 122 + 1);
+//    }
+
+//    passwordBuilder.Append(c);
+//}
+
+//string password = passwordBuilder.ToString();
+
+//Console.WriteLine(password);
+////////
+
+
+
+//{
+//    int[] arr = { 10, 20, 30 };
+//    //int[] arr2 = new[] { 10, 20, 30 };
+//    //int[] arr3 = new int[3] { 10, 20, 30 };
+
+//    int lastIndex = arr.GetUpperBound(0);
+
+//    Console.WriteLine(lastIndex);
+//}
+
+//{
+//    string?[]? names = { "vadik", "vladik", null, "semen"};
+
+//    foreach (var name in names)
+//    {
+//        var result = name?.Capitalize(); // null conditional
+
+//        Console.WriteLine(result);
+//    }
+//}
+
+
+/*{
+    string? text = null;
+
+    // text = text ?? "123";
+    text ??= "123";
+
+    Console.WriteLine(text ?? "Text is null!!!");
+
+    //if (text == null)
+    //{
+    //    Console.WriteLine("Text is null!!!");
+    //}
+    //else
+    //{
+    //    Console.WriteLine(text);
+    //}
+
+}*/
+////////////////////
+
+//{
+//    string?[]? names = { "vadik", "vladik", null, "semen" };
+
+//    names[2] = "Egor";
+
+//    Console.WriteLine($"Length = {names.Length}");
+//    Console.WriteLine($"Rank = {names.Rank}");
+//}
+
 {
-    int isDigit = random.Next(0, 2);
-    char c;
+    //int[,] matrix = new int[,]
+    //{
+    //    { 10, 20, 30, 20 },
+    //    { 90, 42, 36, 10 }
+    //};
 
-    if (isDigit == 1)
-    {
-        c = (char)random.Next(48, 57 + 1);
-    }
-    else
-    {
-        c = (char)random.Next(97, 122 + 1);
-    }
+    //Console.WriteLine(matrix.GetLowerBound(0));
+    //Console.WriteLine(matrix.GetLowerBound(1));
 
-    passwordBuilder.Append(c);
+    //int rows = matrix.GetUpperBound(0) + 1;
+    //int cols = matrix.GetUpperBound(1) + 1;
+
+    //Console.WriteLine(rows);
+    //Console.WriteLine(cols);
+
+    //for (int i = 0; i < rows; ++i)
+    //{
+    //    for (int j = 0; j < cols; ++j)
+    //    {
+    //        matrix[i, j] *= 2;
+
+    //        Console.Write($"{matrix[i, j]} ");
+    //    }
+    //    Console.WriteLine();
+    //}
 }
 
-string password = passwordBuilder.ToString();
+///////////////////////
 
-Console.WriteLine(password);
+{
+    int[][] jagged = new int[][]
+    {
+        new int[] { 30, 42, 50 },
+        new int[] { 90, 80 },
+        new int[] { 90, 80, 70, 90, 100 }
+    };
+
+
+    for (int i = 0; i < jagged.Length; i++)
+    {
+        for (int j = 0; j < jagged[i].Length; j++)
+        {
+            Console.Write($"{jagged[i][j]} ");
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+}
+
+
+public static class StringExtensions
+{
+    public static string Capitalize(this string source)
+    {
+        return $"{char.ToUpper(source[0])}{source[1..]}";
+    }
+}

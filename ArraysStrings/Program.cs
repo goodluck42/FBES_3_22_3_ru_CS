ffff17d2262af27d15493562374e97d8c2977729
xrlogic1 <xrlogic1@gmail.com>
2023-08-25 20:18:23 +0400

    *

---
+++ ArraysStrings/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+
+//int version = 20;
+
+//string s = $"   C++{version}";
+//string str = "C++" + 20;
+
+//Console.WriteLine(s.Contains("C++"));
+
+//string newStr = s.TrimStart();
+
+
+// s.IndexOf("C++");
+
+//Console.WriteLine("--------------");
+//Console.WriteLine(str[^1]);
+//Console.WriteLine(str[0..3]);
+//Console.WriteLine("--------------");
+
+//Console.WriteLine(newStr);
+//Console.WriteLine(s);
+//Console.WriteLine(str);
+
+//Console.WriteLine(f"{}");
+
+//Console.WriteLine("--------------");
+//////////////////////
+
+//string file = "homework.txt";
+//string rawStr = $@"C:\Users\skiba_al\Desktop\{file}";
+
+//Console.WriteLine(rawStr);
+//////////////////////
+
+//Random random = new Random();
+
+//string password = string.Empty;
+
+//for (int i = 0; i < 12; i++)
+//{
+//    int isDigit = random.Next(0, 2);
+//    char c;
+
+
+//    if (isDigit == 1)
+//    {
+//        c = (char)random.Next(48, 57 + 1);
+//    }
+//    else
+//    {
+//        c = (char)random.Next(97, 122 + 1);
+//    }
+
+//    password += c;
+//}
+
+//Console.WriteLine(password);
+
+//////////////////////////////
+
+// using PNSE = System.PlatformNotSupportedException;
+
+///Random random = new Random();
+var random = new Random();
+var passwordBuilder = new StringBuilder();
+
+for (int i = 0; i < 12; i++)
+{
+    int isDigit = random.Next(0, 2);
+    char c;
+
+    if (isDigit == 1)
+    {
+        c = (char)random.Next(48, 57 + 1);
+    }
+    else
+    {
+        c = (char)random.Next(97, 122 + 1);
+    }
+
+    passwordBuilder.Append(c);
+}
+
+string password = passwordBuilder.ToString();
+
+Console.WriteLine(password);
---
+++ FirstNETApp/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+Person p1 = new Person();
+
+p1.Age = 10; // Setter
+
+Console.WriteLine(p1.Age); // Getter
+ 
+
+
+//p1.Age = -10;
+//p1.FirstName = "Semen";
+//p1.LastName = "Petrovich";
+
+//Console.WriteLine(p1.Age);
+//Console.WriteLine(p1.FirstName);
+//Console.WriteLine(p1.LastName);
+
+//Console.WriteLine("Hello, World!"); // top level statements
+
+//bool b = true; // System.Boolean
+//byte bt = 1; // 0 - 255
+//sbyte sbt = 2; // -128 - 127
+//char c = 'й';
+//char c1 = 'w';
+//short s3 = 10;
+//int i = 0; // System.Int32
+//long l = 10L; // System.Int64
+//ulong ul = 10UL; // System.UInt64
+//float f = 10F; // 4 bytes // System.Half
+//double d = 20.2; // 8 bytes // System.Double
+//decimal dm = 20m; // 16 bytes // System.Decimal
+//string str = "Hello C#";
+
+//// char c3 = 90; // error
+
+//Console.WriteLine(c);
+//Console.WriteLine(c1);
+
+
+// 1
+//string? read = Console.ReadLine();
+
+//try
+//{
+//    int result = int.Parse(read);
+//    Console.WriteLine(result * 2);
+//}
+//catch (FormatException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+// 2
+
+//string? read = Console.ReadLine();
+
+//if (int.TryParse(read, out int value))
+//{
+//    Console.WriteLine(value * 2);
+//}
+
+// 3
+//Console.WriteLine(0.1m + 0.2m);
+//Console.WriteLine("Enter first number: ");
+//string? input1 = Console.ReadLine();
+//Console.WriteLine("Enter operator number: ");
+//string? input2 = Console.ReadLine();
+//Console.WriteLine("Enter second number: ");
+//string? input3 = Console.ReadLine();
+
+//if (input1 != null && input2 != null && input3 != null)
+//{
+//    float num1 = float.Parse(input1);
+//    string op = input2;
+//    float num2 = float.Parse(input3);
+
+//    switch (op)
+//    {
+//        case "+":
+//            Console.WriteLine(num1 + num2);
+//            break;
+
+//        case "-":
+//            Console.WriteLine(num1 - num2);
+//            break;
+//        case "*":
+//            Console.WriteLine(num1 * num2);
+//            break;
+//        case "/":
+//            if (num2 != 0)
+//            {
+//                Console.WriteLine(num1 / num2);
+//            }
+//            else
+//            {
+//                Console.WriteLine("∞");
+//            }
+//            break;
+//        case "++":
+//            goto case "+";
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
+
+
+// ReferenceType (classes) (pass by reference) (in heap)
+// ValueType (structs) (pass by copy) (in stack)
+
+// string - reference type & immutable
+
+class Person
+{
+    private int age; // field
+
+    public int Age // property
+    {
+        get
+        {
+            Console.WriteLine("Getter called!");
+
+            return age;
+        }
+        set
+        {
+            Console.WriteLine("Setter called!");
+            if (value < 0)
+            {
+                age = 0;
+            }
+            else
+            {
+                age = value;
+            }
+        }
+    }
+    public string? FirstName { get; set; }  // auto property
+    public string? LastName { get; set; } // auto property
+}
+

24c568e9923304c12b8868b8f59809c1387b589a
xrlogic1 <xrlogic1@gmail.com>
2023-09-20 21:50:56 +0400

    *

---
+++ FileSystem/Program.cs
@@ -0,0 +1,203 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+
+{
+    //// WRITE
+    // var fileStream = new FileStream("data.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    //
+    // byte[] buffer = Encoding.UTF8.GetBytes("üüü");
+    //
+    // fileStream.Write(buffer, 0, buffer.Length);
+    //
+    // fileStream.Close();
+
+
+    //// READ
+    // string fileName = "data.bin";
+    //
+    // using var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+    //
+    // FileInfo info = new FileInfo(fileName);
+    //
+    // Console.WriteLine(info.Length);
+    //
+    // byte[] buffer = new byte[info.Length];
+    //
+    // fileStream.Read(buffer, 0, (int)info.Length);
+    //
+    // string resultString = Encoding.UTF8.GetString(buffer);
+    //
+    // Console.OutputEncoding = Encoding.UTF8;
+    // Console.InputEncoding = Encoding.UTF8;
+    //
+    // Console.WriteLine(resultString);
+}
+
+
+{
+    using var obj = new UnmanagedObject();
+}
+
+{
+    var order = new Order()
+    {
+        Id = 1,
+        DateOrdered = DateTime.Now,
+        DateDelivered = DateTime.Now,
+        Products = new()
+        {
+            new Product()
+            {
+                Name = "Logitech Mouse",
+                Count = 1,
+                Price = 100
+            },
+            new Product()
+            {
+                Name = "HyperX Keyboard",
+                Count = 1,
+                Price = 150
+            },
+            new Product()
+            {
+                Name = "CPU",
+                Count = 1,
+                Price = 200
+            },
+        }
+    };
+
+
+    // using var fileStream = new FileStream("users.json", FileMode.Open);
+
+    //// WRITE JSON
+    // var fileStream = File.Open("users.json", FileMode.Open);
+    //
+    // using var streamWriter = new StreamWriter("orders.json");
+    //
+    // var json = JsonSerializer.Serialize(order, new JsonSerializerOptions()
+    // {
+    //     WriteIndented = true
+    // });
+    //
+    // streamWriter.Write(json);
+    // streamWriter.Flush();
+
+    // READ JSON
+
+    // using var streamReader = new StreamReader("orders.json");
+    // var json = streamReader.ReadToEnd();
+    // var read = JsonSerializer.Deserialize<Order>(json);
+    //
+    // Console.WriteLine(read);
+}
+
+{
+    // var order = new Order()
+    // {
+    //     Id = 1,
+    //     DateOrdered = DateTime.Now,
+    //     DateDelivered = DateTime.Now,
+    //     Products = new()
+    //     {
+    //         new Product()
+    //         {
+    //             Name = "Logitech Mouse",
+    //             Count = 1,
+    //             Price = 100
+    //         },
+    //         new Product()
+    //         {
+    //             Name = "HyperX Keyboard",
+    //             Count = 1,
+    //             Price = 150
+    //         },
+    //         new Product()
+    //         {
+    //             Name = "CPU",
+    //             Count = 1,
+    //             Price = 200
+    //         },
+    //     }
+    // };
+    //
+    //
+    // using var fileStream = new FileStream("data.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    // using var reader = new StreamReader(fileStream);
+    // using var writer = new StreamWriter(fileStream);
+    //
+    // int choice = int.Parse(Console.ReadLine()!);
+    //
+    // if (choice == 1)
+    // {
+    //     var json = JsonSerializer.Serialize(order, new JsonSerializerOptions()
+    //     {
+    //         WriteIndented = true
+    //     });
+    //
+    //     writer.Write(json);
+    //     writer.Flush();
+    // }
+    // else if (choice == 2)
+    // {
+    //     var json = reader.ReadToEnd();
+    //     var read = JsonSerializer.Deserialize<Order>(json);
+    //
+    //     Console.WriteLine(read);
+    // }
+}
+
+if (Directory.Exists(@"C:\Windows"))
+{
+    Console.WriteLine("Exists");
+}
+
+var folders = Directory.GetDirectories(@"C:\Users\skiba_al\source\repos\FBES_3_22_3_ru_CS");
+
+foreach (var folder in folders)
+{
+    Console.WriteLine(folder);
+}
+
+var files = Directory.GetFiles(@"C:\MinGW\bin");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+
+
+
+class UnmanagedObject : IDisposable
+{
+    public UnmanagedObject()
+    {
+        Data = Marshal.AllocHGlobal(120);
+    }
+
+    public IntPtr Data { get; }
+
+    public void Dispose()
+    {
+        Marshal.FreeHGlobal(Data);
+    }
+}
+
+record Product
+{
+    public string? Name { get; set; }
+    public int Count { get; set; }
+    public int Price { get; set; }
+}
+
+record Order
+{
+    public int Id { get; set; }
+    public List<Product> Products { get; set; } = null!;
+    public DateTime DateOrdered { get; set; }
+    public DateTime DateDelivered { get; set; }
+}
---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,48 @@
+﻿// var list = new List<Person>
+// {
+//     new Person() // p1
+//     {
+//         Age = 18,
+//         Name = "Vadik"
+//     },
+//     new Person() // p2
+//     {
+//         Age = 20,
+//         Name = "Vlad"
+//     },
+//     new Person() // p3
+//     {
+//         Age = 22,
+//         Name = "Semen",
+//         Friend = null
+//     }
+// };
+//
+// list[0].Friend = list[1]; // p1.Friend = p2;
+// list[1].Friend = list[0]; // p2.Friend = p1;
+//
+// list[0].Name = "Nikita";
+//
+// list.Remove(list[1]); // p2
+// list.Remove(list[0]); // p1
+//
+// list[0].Friend = null;
+// Console.WriteLine(list[0].Friend.Age);
+// Console.WriteLine(list[0].Friend.Name);
+
+while (true)
+{
+    var p1 = new Person();
+    
+}
+
+class Person
+{
+    ~Person()
+    {
+        Console.WriteLine("Destroyed");
+    }
+    public int Age { get; set; }
+    public string? Name { get; set; }
+    public Person? Friend { get; set; }
+}
